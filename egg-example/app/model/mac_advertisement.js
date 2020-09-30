23eff762277200694ab5b4fc36f7813084aaf4d2
Your Name <you@example.com>
2020-09-30 19:54:54 +0800

    新增广告位数据库映射model

---
+++ egg-example/app/model/mac_advertisement.js
@@ -0,0 +1,37 @@
+/* indent size: 2 */
+
+module.exports = app => {
+  const DataTypes = app.Sequelize;
+
+  const Model = app.model.define('mac_advertisement', {
+    id: {
+      type: DataTypes.INTEGER(8),
+      allowNull: false,
+      primaryKey: true
+    },
+    url_img: {
+      type: DataTypes.STRING(255),
+      allowNull: false
+    },
+    url: {
+      type: DataTypes.STRING(255),
+      allowNull: false
+    },
+    status: {
+      type: DataTypes.INTEGER(8),
+      allowNull: false
+    },
+    jump: {
+      type: DataTypes.INTEGER(8),
+      allowNull: false
+    }
+  }, {
+    tableName: 'mac_advertisement'
+  });
+
+  Model.associate = function() {
+
+  }
+
+  return Model;
+};
