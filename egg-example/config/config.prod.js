8b2285a7447a58070588e221aaf6da9d2abb5326
Your Name <you@example.com>
2020-12-10 22:13:17 +0800

    egg start挂起

--- egg-example/config/config.prod.js
+++ egg-example/config/config.prod.js
@@ -3,15 +3,15 @@ module.exports = () => {
     // 单数据库信息配置	
     client: {
       // host
-      host: 'localhost',
+      host: '117.50.17.60',
       // 端口号
-      port: '3306',
+      port: 3306,
       // 用户名
-      user: 'root',
+      user: 'sql0_0_0_0',
       // 密码
-      password: 'root',
+      password: 'MmxAS7xZQy',
       // 数据库名
-      database: 'root',
+      database: 'sql0_0_0_0',
     },	
     // 是否加载到 app 上，默认开启	
     app: true,	
