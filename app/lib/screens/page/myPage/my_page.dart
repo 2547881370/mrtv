9bbd6d14039ea21ea8a17ed8cca896a993017362
Your Name <you@example.com>
2020-12-13 12:57:13 +0800

    app 登录

--- app/lib/screens/page/myPage/my_page.dart
+++ app/lib/screens/page/myPage/my_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:app/components/home/background_component.dart';
+import 'package:flutter_screenutil/screenutil.dart';
+
+import '../../../constants.dart';
 
 class MyPage extends StatefulWidget {
   MyPage({Key key}) : super(key: key);
@@ -10,12 +14,28 @@ class MyPage extends StatefulWidget {
 class _MyPageState extends State<MyPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-       child: Center(
-         child:Text(
-           "我的"
-         )
-       ),
+    final double topPadding =
+        MediaQuery.of(context).padding.top + kDefaultPaddin + 15;
+    var container = Container(
+                    width: ScreenUtil().setWidth(750),
+                    height: ScreenUtil().setHeight(90),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        color: Colors.deepOrangeAccent.withOpacity(0.1),
+                    ),
+                    child: Text("我的",style: TextStyle(color: Colors.white,fontSize: ScreenUtil().setSp(30)),),
+                  );
+    return Scaffold(
+      body: BackgroundComponent(
+          child: Container(
+              margin: EdgeInsets.only(top: topPadding),
+              child : Column(
+                children: <Widget>[
+                  container
+                ],
+              )
+          ),
+      ),
     );
   }
 }
