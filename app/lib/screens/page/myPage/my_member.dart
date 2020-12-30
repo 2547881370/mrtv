3d9bee7c8ab91607d7a733cefd3eb470458c8cbb
Your Name <you@example.com>
2020-12-30 22:33:17 +0800

    在线充值积分-充值页面

--- app/lib/screens/page/myPage/my_member.dart
+++ app/lib/screens/page/myPage/my_member.dart
@@ -28,12 +28,99 @@ class _MyMenmBerState extends State<MyMenmBer> {
             child: MyPageTitle(),
           ),
           MyMenmBerNumber(),
-         Container(
-           padding: EdgeInsets.symmetric(vertical:ScreenUtil().setHeight(25)),
-           child: CustomTabBar()
-           )
+          SizedBox(
+            height:ScreenUtil().setHeight(5),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(vertical:ScreenUtil().setHeight(25)),
+            child: CustomTabBar()
+          ),
+          MyMenbBerInput(title:"充值金额",hintText : "请输入要充值的金额",tips:Text("最小充值金额1元,1元可兑换10积分")),
+          SizedBox(
+            height:ScreenUtil().setHeight(60),
+          ),
+          MyMenbBerInput(title:"卡密充值积分",hintText : "输入粘贴密码",tips:Row(children: [
+            Text("友情提示"),
+            InkWell(
+              child:Text("点击进入在线充值链接",style:TextStyle(color:Colors.blue[600])),
+              onTap:()=>{
+                print("跳转webView")
+              }
+            )
+          ],)),
           ],
         )
+      )
+    );
+  }
+}
+
+class MyMenbBerInput extends StatelessWidget {
+  final String title;
+  final String hintText;
+  final Widget tips;
+  const MyMenbBerInput({
+    Key key,
+    @required this.title,
+    @required this.hintText,
+    @required this.tips,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: ScreenUtil().setWidth(750),
+      child:Column(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal:ScreenUtil().setWidth(20)),
+            decoration: BoxDecoration(
+              border: Border.all(  width: 1, //宽度
+                color: Colors.black12),
+                borderRadius: BorderRadius.all(Radius.circular(15))
+            ),
+            child:Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(title,style:TextStyle(color:Colors.black,fontSize: ScreenUtil().setSp(25))),
+                Container(margin:EdgeInsets.symmetric(horizontal:ScreenUtil().setWidth(10)), child: Text("|" , style:TextStyle(color:Colors.black,fontSize: ScreenUtil().setSp(25))),),
+                Expanded(
+                  child: TextField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: hintText,
+                    hintStyle: TextStyle(color: Colors.grey),
+                    hintMaxLines: 1
+                  ),
+                 ),
+                )
+              ]
+            )
+          ),
+           SizedBox(
+            height:ScreenUtil().setHeight(10),
+          ),
+          Container(
+            padding:EdgeInsets.symmetric(horizontal:ScreenUtil().setWidth(15),vertical:ScreenUtil().setHeight(10)),
+            alignment: Alignment.centerLeft,
+            child:tips
+          ),
+           SizedBox(
+            height:ScreenUtil().setHeight(10),
+          ),
+         RaisedButton(
+          child: Container(
+            padding: EdgeInsets.symmetric(vertical:ScreenUtil().setHeight(10),horizontal:ScreenUtil().setWidth(20)),
+            alignment: Alignment.center,
+            child : Text("确定",style:TextStyle(color:Colors.white,fontSize: ScreenUtil().setSp(27)))
+          ),
+          shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10)),
+          onPressed: () {},
+          color: Color.fromRGBO(255, 102, 57, 1),
+          textColor:Colors.white,
+          ),
+        ]
       )
     );
   }
@@ -48,6 +135,10 @@ class _CustomTabBarState extends State<CustomTabBar>
     with SingleTickerProviderStateMixin {
   final tabs = ['在线充值积分', '积分升级会员'];
   TabController _tabController;
+
+  final GlobalKey globalKey = GlobalKey();
+
+
 
   @override
   void initState() {
@@ -80,7 +171,8 @@ class _CustomTabBarState extends State<CustomTabBar>
             indicatorColor: Colors.orangeAccent,
             tabs: tabs
                 .map((e) => Container(
-                      width: ScreenUtil().setWidth(290),
+                  width: ScreenUtil().setWidth(270),
+                     alignment: Alignment.center,
                       child: Tab(text: e),
                     ))
                 .toList(),
